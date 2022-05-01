f62107e7b4adfa8e0c238775dd27e6e791c6543a
Schwaaa <99062653@mydavinci.nl>
2022-05-01 18:29:36 +0200

    0.30: shuffling population

--- Pokemons/Pokemons.cs
+++ Pokemons/Pokemons.cs
@@ -4,6 +4,8 @@ namespace Pokemons
 	{
 		public static List<Pokemon> Population = new List<Pokemon>();
 
+		
+
 	}
 	class Pokemon : Pokemons
 	{
@@ -34,13 +36,35 @@ namespace Pokemons
 	{
 		public static void InitializePokemons()
 		{
-			Console.WriteLine("Initializing Pokemon...");
+			Console.WriteLine("Initializing Pokemons...");
+
 			Pokemon Pikachu = new Pokemon("Pikachu", "Electric", 100, 100, new Dictionary<string, int>() { { "Thunder Shock", 50 } }, new Dictionary<string, int>() { { "Electric", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
 			Pokemon Charmander = new Pokemon("Charmander", "Fire", 100, 100, new Dictionary<string, int>() { { "Ember", 50 } }, new Dictionary<string, int>() { { "Fire", 50 } }, new Dictionary<string, int>() { { "Water", 50 } });
 			Pokemon Squirtle = new Pokemon("Squirtle", "Water", 100, 100, new Dictionary<string, int>() { { "Bubble", 50 } }, new Dictionary<string, int>() { { "Water", 50 } }, new Dictionary<string, int>() { { "Grass", 50 } });
+			
 			Pokemon Bulbasaur = new Pokemon("Bulbasaur", "Grass", 100, 100, new Dictionary<string, int>() { { "Vine Whip", 50 } }, new Dictionary<string, int>() { { "Grass", 50 } }, new Dictionary<string, int>() { { "Water", 50 } });
 			Pokemon Pidgey = new Pokemon("Pidgey", "Normal", 100, 100, new Dictionary<string, int>() { { "Gust", 50 } }, new Dictionary<string, int>() { { "Normal", 50 } }, new Dictionary<string, int>() { { "Flying", 50 } });
 			Pokemon Rattata = new Pokemon("Rattata", "Normal", 100, 100, new Dictionary<string, int>() { { "Tackle", 50 } }, new Dictionary<string, int>() { { "Normal", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
+			
+			Pokemon Arbok = new Pokemon("Arbok", "Poison", 100, 100, new Dictionary<string, int>() { { "Bite", 50 } }, new Dictionary<string, int>() { { "Poison", 50 } }, new Dictionary<string, int>() { { "Ground", 50 } });
+			Pokemon Butterfree = new Pokemon("Butterfree", "Bug", 100, 100, new Dictionary<string, int>() { { "Confusion", 50 } }, new Dictionary<string, int>() { { "Bug", 50 } }, new Dictionary<string, int>() { { "Flying", 50 } });
+			Pokemon Jigglypuff = new Pokemon("Jigglypuff", "Normal", 100, 100, new Dictionary<string, int>() { { "Sing", 50 } }, new Dictionary<string, int>() { { "Normal", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
+			
+			Pokemon Zubat = new Pokemon("Zubat", "Poison", 100, 100, new Dictionary<string, int>() { { "Bite", 50 } }, new Dictionary<string, int>() { { "Poison", 50 } }, new Dictionary<string, int>() { { "Flying", 50 } });
+			Pokemon Geodude = new Pokemon("Geodude", "Rock", 100, 100, new Dictionary<string, int>() { { "Rock Throw", 50 } }, new Dictionary<string, int>() { { "Rock", 50 } }, new Dictionary<string, int>() { { "Ground", 50 } });
+			Pokemon Onix = new Pokemon("Onix", "Rock", 100, 100, new Dictionary<string, int>() { { "Rock Throw", 50 } }, new Dictionary<string, int>() { { "Rock", 50 } }, new Dictionary<string, int>() { { "Ground", 50 } });
+
+			Pokemon Pidgeot = new Pokemon("Pidgeot", "Flying", 100, 100, new Dictionary<string, int>() { { "Steel Wing", 50 } }, new Dictionary<string, int>() { { "Flying", 50 } }, new Dictionary<string, int>() { { "Ground", 50 } });
+			Pokemon Magikarp = new Pokemon("Magikarp", "Water", 100, 100, new Dictionary<string, int>() { { "Splash", 50 } }, new Dictionary<string, int>() { { "Water", 50 } }, new Dictionary<string, int>() { { "Electric", 50 } });
+			Pokemon Eevee = new Pokemon("Eevee", "Normal", 100, 100, new Dictionary<string, int>() { { "Quick Attack", 50 } }, new Dictionary<string, int>() { { "Normal", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
+
+			Pokemon Charizard = new Pokemon("Charizard", "Fire", 100, 100, new Dictionary<string, int>() { { "Fire Blast", 50 } }, new Dictionary<string, int>() { { "Fire", 50 } }, new Dictionary<string, int>() { { "Water", 50 } });
+			Pokemon Blastoise = new Pokemon("Blastoise", "Water", 100, 100, new Dictionary<string, int>() { { "Hydro Pump", 50 } }, new Dictionary<string, int>() { { "Water", 50 } }, new Dictionary<string, int>() { { "Grass", 50 } });
+			Pokemon Venusaur = new Pokemon("Venusaur", "Grass", 100, 100, new Dictionary<string, int>() { { "Vine Whip", 50 } }, new Dictionary<string, int>() { { "Grass", 50 } }, new Dictionary<string, int>() { { "Water", 50 } });
+
+			Pokemon Mewtwo = new Pokemon("Mewtwo", "Psychic", 100, 100, new Dictionary<string, int>() { { "Psychic", 50 } }, new Dictionary<string, int>() { { "Psychic", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
+			Pokemon Nidoqueen = new Pokemon("Nidoqueen", "Poison", 100, 100, new Dictionary<string, int>() { { "Bite", 50 } }, new Dictionary<string, int>() { { "Poison", 50 } }, new Dictionary<string, int>() { { "Ground", 50 } });
+			Pokemon Vulpixtra = new Pokemon("Vulpixtra", "Electric", 100, 100, new Dictionary<string, int>() { { "Thunder Shock", 50 } }, new Dictionary<string, int>() { { "Electric", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,20 @@
 
 class Program 
 {
+
+	// de reden dat dit zo naar links is omdat anders de ascii raar gaat doen
+	public static string Logo = @" 
+______     _       ______       _   _   _      
+| ___ \   | |      | ___ \     | | | | | |     
+| |_/ /__ | | _____| |_/ / __ _| |_| |_| | ___ 
+|  __/ _ \| |/ / _ \ ___ \/ _` | __| __| |/ _ \
+| | | (_) |   <  __/ |_/ / (_| | |_| |_| |  __/
+\_|  \___/|_|\_\___\____/ \__,_|\__|\__|_|\___|
+	";
 	public static void Settings()
 	{
 		Console.Title = "Pokemon Battle"; //title van de console
 		Console.CursorVisible = true; //toont de cursor
-		Console.BackgroundColor = ConsoleColor.Red; //background color van de console: Black, White, Red etca
-		Console.ForegroundColor = ConsoleColor.White; //foreground color van de console: Black, White, Red etca
 
 		//UNUSED
 		//Console.Beep(); //speelt een beepje af
@@ -15,13 +23,15 @@ class Program
 	public static void Main()
 	{
 		Program.Settings();
+
+		Console.WriteLine(Logo);
 		Init.InitializePokemons();
-		
+		Pokemon.Population = Pokemon.Population.OrderBy(i => Guid.NewGuid()).ToList(); //shuffle de lijst -> guid is een 128 character die nooit meer gebruikt wordt
 		foreach (var pokemon in Pokemon.Population)
 		{
 			Console.WriteLine(pokemon.Name);
+			Thread.Sleep(500); // wacht 0.5 seconde
 		}
 		Console.ReadKey();
-
 	}
 }
