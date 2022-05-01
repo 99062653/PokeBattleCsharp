60b445edc5739875007f779b10bebd50696426d4
Schwaaa <99062653@mydavinci.nl>
2022-05-01 19:08:00 +0200

    0.35: alot of testing

--- Program.cs
+++ Program.cs
@@ -12,26 +12,45 @@ ______     _       ______       _   _   _
 | | | (_) |   <  __/ |_/ / (_| | |_| |_| |  __/
 \_|  \___/|_|\_\___\____/ \__,_|\__|\__|_|\___|
 	";
-	public static void Settings()
+	
+	public void Settings()
 	{
 		Console.Title = "Pokemon Battle"; //title van de console
 		Console.CursorVisible = true; //toont de cursor
+		Console.BackgroundColor = ConsoleColor.Black; //background color van de console
 
 		//UNUSED
 		//Console.Beep(); //speelt een beepje af
 	}
+
+	public void getInfo()
+	{
+
+	}
+
+	public void choosePokemon()
+	{
+
+	}
+
 	public static void Main()
 	{
-		Program.Settings();
-
+		Program program = new Program();
+		program.Settings();
 		Console.WriteLine(Logo);
 		Init.InitializePokemons();
-		Pokemon.Population = Pokemon.Population.OrderBy(i => Guid.NewGuid()).ToList(); //shuffle de lijst -> guid is een 128 character die nooit meer gebruikt wordt
+
 		foreach (var pokemon in Pokemon.Population)
 		{
 			Console.WriteLine(pokemon.Name);
-			Thread.Sleep(500); // wacht 0.5 seconde
+			Thread.Sleep(50); // laat het ff wachten
 		}
-		Console.ReadKey();
+		Console.WriteLine("Klaar! Druk op ENTER om verder te gaan.");
+		ConsoleKey Button = Console.ReadKey().Key; //.Key is de knop die gedrukt is
+
+		if (Button == ConsoleKey.Enter) 
+		{
+
+		}
 	}
 }
