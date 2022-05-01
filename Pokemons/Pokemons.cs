920dc61840b12d26395733c01791048f573d7eb6
- <99062653@mydavinci.nl>
2022-05-01 14:01:36 +0200

    0.1: add initializer

--- Pokemons/Pokemons.cs
+++ Pokemons/Pokemons.cs
@@ -2,7 +2,7 @@ namespace Pokemons
 {
 	class Pokemons 
 	{
-		public static string[] Population; 
+		public static List<Pokemon> Population = new List<Pokemon>();
 
 	}
 	class Pokemon : Pokemons
@@ -25,14 +25,22 @@ namespace Pokemons
 			this.Weakness = weakness;
 			this.Resistance = resistance;
 
+			Pokemon.Population.Add(this);
 		}
+		
 	}
-	
-	class Initialize 
-	{
-		public static void Init()
-		{
 
+	class Init
+	{
+		public static void InitializePokemons()
+		{
+			Console.WriteLine("Initializing Pokemon...");
+			Pokemon Pikachu = new Pokemon("Pikachu", "Electric", 100, 100, new Dictionary<string, int>() { { "Thunder Shock", 50 } }, new Dictionary<string, int>() { { "Electric", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
+			Pokemon Charmander = new Pokemon("Charmander", "Fire", 100, 100, new Dictionary<string, int>() { { "Ember", 50 } }, new Dictionary<string, int>() { { "Fire", 50 } }, new Dictionary<string, int>() { { "Water", 50 } });
+			Pokemon Squirtle = new Pokemon("Squirtle", "Water", 100, 100, new Dictionary<string, int>() { { "Bubble", 50 } }, new Dictionary<string, int>() { { "Water", 50 } }, new Dictionary<string, int>() { { "Grass", 50 } });
+			Pokemon Bulbasaur = new Pokemon("Bulbasaur", "Grass", 100, 100, new Dictionary<string, int>() { { "Vine Whip", 50 } }, new Dictionary<string, int>() { { "Grass", 50 } }, new Dictionary<string, int>() { { "Water", 50 } });
+			Pokemon Pidgey = new Pokemon("Pidgey", "Normal", 100, 100, new Dictionary<string, int>() { { "Gust", 50 } }, new Dictionary<string, int>() { { "Normal", 50 } }, new Dictionary<string, int>() { { "Flying", 50 } });
+			Pokemon Rattata = new Pokemon("Rattata", "Normal", 100, 100, new Dictionary<string, int>() { { "Tackle", 50 } }, new Dictionary<string, int>() { { "Normal", 50 } }, new Dictionary<string, int>() { { "Fighting", 50 } });
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ class Program
 {
 	public static void Main()
 	{
-		Initialize.Init();
+		Init.InitializePokemons();
+		foreach (var pokemon in Pokemon.Population)
+		{
+			Console.WriteLine(pokemon.Name);
+		}
 
-		Pokemon Pikachu = new Pokemon("Pikachu", "Electric", 100, 100, new Dictionary<string, int>() { { "Thunder Shock", 50 } }, new Dictionary<string, int>() { { "Electric", 0 } }, new Dictionary<string, int>() { { "Fighting", 0 } });
-
-		Console.WriteLine(Pikachu.Name);
 	}
 }
