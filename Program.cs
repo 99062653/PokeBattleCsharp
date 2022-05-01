92c16de466825cedde0030b576fb3cb459d5c01d
- <99062653@mydavinci.nl>
2022-05-01 14:19:49 +0200

    0.25: settings

--- Program.cs
+++ Program.cs
@@ -2,9 +2,21 @@
 
 class Program 
 {
+	public static void Settings()
+	{
+		Console.Title = "Pokemon Battle"; //title van de console
+		Console.CursorVisible = true; //toont de cursor
+		Console.BackgroundColor = ConsoleColor.Red; //background color van de console: Black, White, Red etca
+		Console.ForegroundColor = ConsoleColor.White; //foreground color van de console: Black, White, Red etca
+
+		//UNUSED
+		//Console.Beep(); //speelt een beepje af
+	}
 	public static void Main()
 	{
+		Program.Settings();
 		Init.InitializePokemons();
+		
 		foreach (var pokemon in Pokemon.Population)
 		{
 			Console.WriteLine(pokemon.Name);
